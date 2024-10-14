79a32b8f2e9a8478677804f155fcdc1e15cf74ec
Edward Davies <morgandavies428@gmail.com>
2024-10-14 16:45:15 +0100

    Updated include folder with name that wont be caught by IDE Updated ino filename to make IDE happy

--- overwrite_arduino_lib.sh
+++ overwrite_arduino_lib.sh
@@ -2,4 +2,4 @@
 
 # Arduino demands that all of its components be placed in its libraries folder
 # This script moves all components into that folder and overwrites any existing folder data
-cp -arf src/. $HOME/Arduino/libraries
+cp -arf arduino_libs_copy_dir/. $HOME/Arduino/libraries
