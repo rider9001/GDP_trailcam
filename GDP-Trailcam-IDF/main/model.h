137ed61b2767613b781256be2af4c607754e2c3f
Edward Davies <morgandavies428@gmail.com>
2024-11-27 21:58:13 +0000

    Branched to tflite model integration branch, added new tflite file

---
+++ GDP-Trailcam-IDF/main/model.h
@@ -0,0 +1,9 @@
+/// ------------------------------------------
+/// @file model.h
+///
+/// @brief Header file for interfacing with the tflite model
+/// ------------------------------------------
+#pragma once
+
+extern const unsigned char best_float16_tflite[];
+extern const unsigned int best_float16_tflite_len;
